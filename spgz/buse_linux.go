c66bffd7697afec665c55836a7388346e8a1c2a5
Dmitry Panov <dop@itoolabs.com>
2021-10-20 15:49:03 +0100

    Attempt to run 'insmod nbd' if the device file does not exist

--- spgz/buse_linux.go
+++ spgz/buse_linux.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"os"
+	"os/exec"
 	"os/signal"
 
 	"github.com/dop251/buse"
@@ -15,14 +17,36 @@ func getBuseFlag() *string {
 	return flag.String("b", "", "Connect to a local nbd device")
 }
 
+func checkDev(dev string) error {
+	_, err := os.Stat(dev)
+	if err != nil {
+		if os.IsNotExist(err) {
+			// device does not exist, try running 'modprobe nbd'
+			cmd := exec.Command("modprobe", "nbd")
+			err = cmd.Run()
+			if err != nil {
+				return fmt.Errorf("%q does not exist and 'modprobe nbd' has failed: %w", err)
+			}
+		} else {
+			return err
+		}
+	}
+	return nil
+}
+
 func doBuse(file, dev string) {
 	f, err := spgz.OpenFile(file, os.O_RDWR, 0666)
 	if err != nil {
 		log.Fatalf("Could not open file: %v", err)
 	}
+	defer f.Close()
 	size, err := f.Size()
 	if err != nil {
 		log.Fatalf("Could not get size: %v", err)
+	}
+	err = checkDev(dev)
+	if err != nil {
+		log.Fatal(err)
 	}
 	device, err := buse.NewDevice(dev, size, spgz.NewBuseDevice(f))
 	if err != nil {
